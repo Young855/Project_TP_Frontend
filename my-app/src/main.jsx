315dc94942a81760836476a6ae8ad582de4b7bf3
young855 <yogwo123@gmail.com>
2025-10-23 18:07:15 +0900

    오타 및 오류사항 수정 완료

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,1 +1,12 @@
+// src/main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css'; // 전역 CSS 파일을 여기서 import 합니다.
 
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
